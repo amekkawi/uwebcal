82730910db2d63f11ce6dbd5aa8df683022367d1
Andre Mekkawi <github@andremekkawi.com>
2012-03-06 04:56:28 -0500

    The template layout now throws an error if the calendar's template does not include the "{{content}}" tag.

--- protected/views/layouts/template.php
+++ protected/views/layouts/template.php
@@ -1,2 +1,5 @@
 <?php
+if (strpos($this->calendar['html'].'', '{{content}}') === false)
+	throw new CHttpException(500, Yii::t('app', 'Cannot display calendar template since it is missing the "{tag} tag".', array('{tag}' => '{{content}}')));
+
 echo str_replace('{{content}}', $content, $this->calendar['html'].'');
